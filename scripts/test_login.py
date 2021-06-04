35662b984f68b3ac09886d3d15d0e5887cf00182
xiachangjun <xiachangjun@163.com>
2021-06-04 14:14:14 +0800

    报告

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -1,17 +1,19 @@
+import time
+
 import allure
 from selenium import webdriver
 
 
 class TestLoging:
     def test_login_001(self):
-        print(1)
+        print("日期:{}".format(time.strftime("%Y-%m-%d %H:%M:%S")))
         allure.attach("输入", "123", allure.attachment_type.TEXT)
         assert 1
     def test_login_002(self):
         print(2)
         # driver = webdriver.Chrome()
         # driver.get('http://baidu.com')
-        allure.attach("输入", "123", allure.attachment_type.TEXT)
+        allure.attach("输入", "日期:{}".format(time.strftime("%Y-%m-%d %H:%M:%S")), allure.attachment_type.TEXT)
         # allure.attach("截图", driver.get_screenshot_as_png(), allure.attachment_type.PNG)
 
         assert 1
